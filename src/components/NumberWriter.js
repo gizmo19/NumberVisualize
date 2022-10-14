5255524d41d92d0e116fb0d96423ea2094845cbb
gizmo <axwing@wp.pl>
2022-10-14 11:20:58 +0200

    sorting works but needs to optimize app

--- src/components/NumberWriter.js
+++ src/components/NumberWriter.js
@@ -2,13 +2,15 @@ import React, { useState } from "react";
 
 const NumberWriter = (props) => {
   const height = props.element;
+
   return (
     <div className="columns">
       <div
         className="column"
         key={props.element}
-        style={{ height: height * 100 }}
+        style={{ height: height * 100, width: 30 }}
       >
+        {console.log(document.getElementsByClassName('inputValue'))}
         <h2>{props.element}</h2>
       </div>
     </div>
