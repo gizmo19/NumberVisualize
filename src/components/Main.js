f35482c6b429114ef4fc1f8ed6ff29970d12b44c
gizmo <axwing@wp.pl>
2022-10-18 14:07:17 +0200

    optimized code, less to read

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,111 +1,38 @@
 import React, { useState } from "react";
 import NumberWriter from "./NumberWriter";
+import BubbleSort from "./BubbleSort";
 
 const Main = () => {
   const [elements, setElements] = useState([]);
   const [inputValue, setInputValue] = useState(10);
   const [newElements, setNewElements] = useState([]);
 
-  const inputHandler = (event) => {
+  const inputValueHandler = (event) => {
     const newValue = parseInt(event.target.value);
     setInputValue(newValue);
+    
   };
 
-  const SortingNumbers = () => {
-    const l = elements.length;
-    let historyElements = [[...elements]];
-
-    let animations = [];
-
-    for (let i = 0; i < l - 1; i++) {
-      for (let j = 0; j < l - i - 1; j++) {
-        if (elements[j] > elements[j + 1]) {
-          let swap = elements[j];
-          elements[j] = elements[j + 1];
-          elements[j + 1] = swap;
-          historyElements.push([...elements]);
-
-          animations.push({
-            el1Index: j,
-            el2Index: j + 1,
-            el1: elements[j],
-            el2: elements[j + 1],
-            swap: true,
-            color: "red",
-          });
-          animations.push({
-            el1Index: j,
-            el2Index: j + 1,
-            el1: elements[j],
-            el2: elements[j + 1],
-            swap: false,
-            color: "blue",
-          });
-        } else {
-          animations.push({
-            el1Index: j,
-            el2Index: j + 1,
-            el1: elements[j],
-            el2: elements[j + 1],
-            swap: false,
-            color: "red",
-          });
-          animations.push({
-            el1Index: j,
-            el2Index: j + 1,
-            el1: elements[j],
-            el2: elements[j + 1],
-            swap: false,
-            color: "blue",
-          });
-        }
-      }
-    }
-
-    const bars = document.getElementsByClassName("column");
-    for (let i = 0; i < animations.length; i++) {
-      let frame = animations[i];
-      // console.log(frame);
-      let barOneStyle = bars[frame.el1Index].style;
-      let barTwoStyle = bars[frame.el2Index].style;
-
-      setTimeout(() => {
-        barOneStyle.backgroundColor = animations[i].color;
-        barTwoStyle.backgroundColor = animations[i].color;
-        if (frame.swap) {
-          console.log("zmienianie wysokosci i cyfry");
-          let el1Height = barOneStyle.height;
-          barOneStyle.height = barTwoStyle.height;
-          barTwoStyle.height = el1Height;
-          let el1Var = bars[frame.el1Index].innerHTML;
-          bars[frame.el1Index].innerHTML = bars[frame.el2Index].innerHTML;
-          bars[frame.el2Index].innerHTML = el1Var;
-        } else {
-          console.log("brak zmiany");
-        }
-        // do poprawy kolorowanie
-      }, i * 50);
-    }
-
-    // const historyLength = historyElements.length
-    // setNewElements(historyElements[historyLength-1])
-  };
-
-  const randomHandler = (min, max) => {
+  const numberGenerator = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   };
 
-  const numberGenerator = () => {
+  function arrayPusher() {
     const arr = [];
     for (let i = 0; i < inputValue; i++) {
-      arr.push(randomHandler(1, 10));
+      arr.push(numberGenerator(1, 10));
     }
     setElements(arr);
-  };
+  }
 
   const renderedElements = elements.map((e) => {
-    return <NumberWriter element={e} />;
+    return <NumberWriter inputVal={inputValue} element={e} />;
   });
+
+  const handleClick = (event) => {
+    let bubbleSort = new BubbleSort(elements);
+    bubbleSort.sortingElements(); 
+  }
 
   return (
     <div>
@@ -113,11 +40,11 @@ const Main = () => {
       <input
         type="number"
         value={inputValue}
-        onChange={inputHandler.bind(this)}
+        onChange={inputValueHandler.bind(this)}
         className="inputNumber"
       ></input>
-      <button onClick={numberGenerator.bind(this)}>Generate</button>
-      <button onClick={SortingNumbers.bind(this)}>Sort</button>
+      <button onClick={arrayPusher.bind(this)}>Generate</button>
+      <button onClick={handleClick.bind(this)}>Sort</button>
       {renderedElements}
     </div>
   );
--- src/components/NumberWriter.js
+++ src/components/NumberWriter.js
@@ -1,16 +1,57 @@
-import React, { useState } from "react";
+import React from "react";
 
 const NumberWriter = (props) => {
   const height = props.element;
+  // const inputValue = props.inputValue;
+  // const inputVal = parseInt(inputValue);
+  let marginLeft = 20;
+  let marginRight = 20;
+  let width = 30;
+
+  // switch (inputVal) {
+  //   case inputVal <= 10 && inputVal > 0:
+  //     console.log("0-10" + inputVal);
+  //     break;
+  //   case inputVal > 10 && inputVal <= 100:
+  //     console.log("11-100" + inputVal);
+  //     break;
+  //   case inputVal > 100 && inputVal <= 1000:
+  //     console.log("101-1000" + inputVal);
+  //     break;
+  //   default:
+  //     console.log("default" + inputVal);
+  // }
+
+  // if (inputValue < 10 && inputValue > 0) {
+  //   marginLeft = 20;
+  //   marginRight = 20;
+  //   width = 30;
+  // } else if (inputValue > 10 && inputValue < 100) {
+  //   marginLeft = 10;
+  //   marginRight = 10;
+  //   width = 10;
+  // } else if (inputValue > 100 && inputValue < 1000) {
+  //   marginLeft = 5;
+  //   marginRight = 5;
+  //   width = 5;
+  // } else {
+  //   marginLeft = 1;
+  //   marginRight = 1;
+  //   width = 5;
+  // }
 
   return (
     <div className="columns">
       <div
         className="column"
         key={props.element}
-        style={{ height: height * 100, width: 30 }}
+        style={{
+          height: height * 100,
+          width: width,
+          marginLeft: marginLeft,
+          marginRight: marginRight,
+        }}
       >
-        
         <h2>{props.element}</h2>
       </div>
     </div>
