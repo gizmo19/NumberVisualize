a6b90d57a634c11a8b4f796253ed8e96105a1595
gizmo <axwing@wp.pl>
2022-10-21 13:40:58 +0200

    applied changes from the comments

--- src/components/BubbleSort.js
+++ src/components/BubbleSort.js
@@ -1,79 +1,64 @@
+import AnimateSort from "./AnimateSort";
+
 export default class BubbleSort {
-  constructor(elements) {
-    this.numberElements = elements;
+  constructor(elements, option) {
+    this.unsortedElements = elements;
+    this.howFastOption = option;
   }
 
-  sortingElements() {
-    const elements = this.numberElements;
-    const l = elements.length;
+  sort() {
+    const elements = this.unsortedElements;
+    const option = this.howFastOption;
+    const length = elements.length;
     const historyElements = [[...elements]];
     const animations = [];
-    for (let i = 0; i < l - 1; i++) {
-      for (let j = 0; j < l - i - 1; j++) {
-        if (this.numberElements[j] > elements[j + 1]) {
-          let swap = elements[j];
-          elements[j] = elements[j + 1];
-          elements[j + 1] = swap;
-          historyElements.push([...elements]);
-
-          animations.push({
-            el1Index: j,
-            el2Index: j + 1,
-            el1: elements[j],
-            el2: elements[j + 1],
-            swap: true,
-            color: "red",
-          });
-          animations.push({
-            el1Index: j,
-            el2Index: j + 1,
-            el1: elements[j],
-            el2: elements[j + 1],
-            swap: false,
-            color: "blue",
-          });
-        } else {
-          animations.push({
-            el1Index: j,
-            el2Index: j + 1,
-            el1: elements[j],
-            el2: elements[j + 1],
-            swap: false,
-            color: "red",
-          });
-          animations.push({
-            el1Index: j,
-            el2Index: j + 1,
-            el1: elements[j],
-            el2: elements[j + 1],
-            swap: false,
-            color: "blue",
-          });
+    if(Array.isArray(elements)) {
+      for (let i = 0; i < length - 1; i++) {
+        for (let j = 0; j < length - i - 1; j++) {
+          if (elements[j] > elements[j + 1]) {
+            let swap = elements[j];
+            elements[j] = elements[j + 1];
+            elements[j + 1] = swap;
+            historyElements.push([...elements]);
+  
+            animations.push({
+              el1Index: j,
+              el2Index: j + 1,
+              el1: elements[j],
+              el2: elements[j + 1],
+              swap: true,
+              color: "red",
+            });
+            animations.push({
+              el1Index: j,
+              el2Index: j + 1,
+              el1: elements[j],
+              el2: elements[j + 1],
+              swap: false,
+              color: "blue",
+            });
+          } else {
+            animations.push({
+              el1Index: j,
+              el2Index: j + 1,
+              el1: elements[j],
+              el2: elements[j + 1],
+              swap: false,
+              color: "red",
+            });
+            animations.push({
+              el1Index: j,
+              el2Index: j + 1,
+              el1: elements[j],
+              el2: elements[j + 1],
+              swap: false,
+              color: "blue",
+            });
+          }
         }
       }
-    }
-    this.animateElements(animations);
-  }
-
-  animateElements(animations) {
-    const bars = document.getElementsByClassName("column");
-    for (let i = 0; i < animations.length; i++) {
-      let frame = animations[i];
-      let barOneStyle = bars[frame.el1Index].style;
-      let barTwoStyle = bars[frame.el2Index].style;
-
-      setTimeout(() => {
-        barOneStyle.backgroundColor = animations[i].color;
-        barTwoStyle.backgroundColor = animations[i].color;
-        if (frame.swap) {
-          let el1Height = barOneStyle.height;
-          barOneStyle.height = barTwoStyle.height;
-          barTwoStyle.height = el1Height;
-          let el1Var = bars[frame.el1Index].innerHTML;
-          bars[frame.el1Index].innerHTML = bars[frame.el2Index].innerHTML;
-          bars[frame.el2Index].innerHTML = el1Var;
-        }
-      }, i * 50);
-    }
+    } 
+    let animate = new AnimateSort(animations, option);
+    animate.animateElements();
   }
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,51 +1,89 @@
 import React, { useState } from "react";
-import NumberWriter from "./NumberWriter";
+import ElementRenderer from "./ElementRenderer";
 import BubbleSort from "./BubbleSort";
+import "../index.css";
+import RandomArrayGenerator from "./RandomArrayGenerator";
 
 const Main = () => {
   const [elements, setElements] = useState([]);
-  const [inputValue, setInputValue] = useState(10);
+  const [howManyNumbersValue, sethowManyNumbersValue] = useState(10);
+  const [
+    rangeOfGeneratedNumbersValue,
+    setrangeOfGeneratedNumbersValue,
+  ] = useState(10);
   const [newElements, setNewElements] = useState([]);
 
-  const inputValueHandler = (event) => {
-    const newValue = parseInt(event.target.value);
-    setInputValue(newValue);
-    
-  };
-
-  const numberGenerator = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  };
-
-  function arrayPusher() {
-    const arr = [];
-    for (let i = 0; i < inputValue; i++) {
-      arr.push(numberGenerator(1, 10));
+  const howManyNumbersValueHandler = (event) => {
+    let howManyNumbers = parseInt(event.target.value);
+    if(howManyNumbers > 100) {
+      howManyNumbers = 100;
     }
+    sethowManyNumbersValue(howManyNumbers);
+  };
+
+  const rangeOfGeneratedNumbers = (event) => {
+    let rangeOfNumbers = parseInt(event.target.value);
+    if(rangeOfNumbers > 50) {
+      rangeOfNumbers = 50;
+    }
+    setrangeOfGeneratedNumbersValue(rangeOfNumbers);
+  };
+
+  function arrayCreator() {
+    let arr;
+    let arrayGenerator = new RandomArrayGenerator(howManyNumbersValue, rangeOfGeneratedNumbersValue);
+    arr = arrayGenerator.generateArray();
     setElements(arr);
   }
 
   const renderedElements = elements.map((e) => {
-    return <NumberWriter inputVal={inputValue} element={e} />;
+    return (
+      <ElementRenderer
+        howManyNumbers={howManyNumbersValue}
+        rangeOfGeneratedNumbers={rangeOfGeneratedNumbersValue}
+        element={e}
+      />
+    );
   });
 
   const handleClick = (event) => {
-    let bubbleSort = new BubbleSort(elements);
-    bubbleSort.sortingElements(); 
-  }
+    let option = document.getElementById("howFastId");
+    let value = option.value;
+    let bubbleSort = new BubbleSort(elements, value);
+    bubbleSort.sort();
+  };
 
   return (
     <div>
-      Number generator
-      <input
-        type="number"
-        value={inputValue}
-        onChange={inputValueHandler.bind(this)}
-        className="inputNumber"
-      ></input>
-      <button onClick={arrayPusher.bind(this)}>Generate</button>
-      <button onClick={handleClick.bind(this)}>Sort</button>
-      {renderedElements}
+      <div className="leftPanel">
+        <p>Write how many numbers to generate:</p>
+        <input
+          type="number"
+          value={howManyNumbersValue}
+          onChange={howManyNumbersValueHandler.bind(this)}
+          className="howManyNumberInput"
+          maxLength={3}
+        ></input>
+        <button onClick={arrayCreator.bind(this)}>Generate</button>
+        <p>Write the range of numbers</p>
+        <input
+          type="number"
+          value={rangeOfGeneratedNumbersValue}
+          onChange={rangeOfGeneratedNumbers.bind(this)}
+          className="numberRangeInput"
+          maxLength={3}
+        ></input>
+        <p>Choose the sort speed</p>
+        <select id="howFastId">
+          <option value="slow">Slow</option>
+          <option value="normal">Normal</option>
+          <option value="fast">Fast</option>
+          <option value="veryFast">Very Fast</option>
+          <option value="iamspeed">I am speed</option>
+        </select>
+        <button onClick={handleClick.bind(this)}>Sort</button>
+      </div>
+      <div className="columnDivHolder">{renderedElements}</div>
     </div>
   );
 };
